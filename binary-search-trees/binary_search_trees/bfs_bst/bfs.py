50cf22bef6290c6304470461ddc92417bb60acf1
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2023-02-18 19:23:26 -0800

    feat: adds bst practice problems

---
+++ binary-search-trees/binary_search_trees/bfs_bst/bfs.py
@@ -0,0 +1,25 @@
+'''
+https://replit.com/@adadev/bst-dfs-practice#binary_search_trees/bfs.py
+'''
+
+class TreeNode:
+
+    def __init__(self, key, value=None, left=None, right=None):
+        if not value:
+            value = key
+        self.key = key
+        self.val = value
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return f"TreeNode {self.val}"
+
+
+class Tree:
+
+    def __init__(self):
+        self.root = None
+
+    def bfs(self):
+        pass
---
+++ binary-search-trees/binary_search_trees/dfs_bst/tests/test_bfs.py
@@ -0,0 +1,52 @@
+import pytest
+from binary_search_trees.bfs_bst.bfs import Tree, TreeNode
+
+
+class TreeExtended(Tree):
+
+    def add_helper(self, current_node, new_node):
+        if new_node.key < current_node.key:
+            if not current_node.left:
+                current_node.left = new_node
+                return
+            self.add_helper(current_node.left, new_node)
+        else:
+            if not current_node.right:
+                current_node.right = new_node
+                return
+            self.add_helper(current_node.right, new_node)
+
+    def add(self, key, value=None):
+        if not self.root:
+            self.root = TreeNode(key, value)
+        else:
+            new_node = TreeNode(key, value)
+            self.add_helper(self.root, new_node)
+
+
+@pytest.fixture()
+def empty_tree() -> TreeExtended():
+    return TreeExtended()
+
+
+@pytest.fixture()
+def tree_with_nodes(empty_tree) -> TreeExtended():
+    empty_tree.add(5, "Peter")
+    empty_tree.add(3, "Paul")
+    empty_tree.add(1, "Mary")
+    empty_tree.add(10, "Karla")
+    empty_tree.add(15, "Ada")
+    empty_tree.add(25, "Kari")
+
+    return empty_tree
+
+
+def test_bfs(tree_with_nodes):
+
+    expected_answer = ["Peter", "Paul", "Karla", "Mary", "Ada", "Kari"]
+
+    answer = tree_with_nodes.bfs()
+    assert answer == expected_answer
+
+
+
